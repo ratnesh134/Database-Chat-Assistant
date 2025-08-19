a270174070a1ae0cd58e1f6f7701381a1892438a
Ratnesh Kumar <ratnesh134@gmail.com>
2025-08-19 15:03:51 +0530

    Database Connection

--- src/app.py
+++ src/app.py
@@ -1,7 +1,15 @@
 from dotenv import load_dotenv
+from langchain_community.utilities import SQLDatabase
 import streamlit as st
 
 load_dotenv()
+
+
+# Function to connect with the database
+def init_database(user: str,password: str, port: str, database: str) -> SQLDatabase:
+    db_uri = f"mysql://{user}:{password}@{host}:{port}/{database}"
+    return SQLDatabase.from_uri(db_uri)
+
 
 st.set_page_config(page_title="Chat With MySQL",page_icon=":speech_balloon")
 
